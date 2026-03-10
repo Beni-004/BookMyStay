9b45facffaf99354d393a4a90cc2be5073509b18
Beni-004 <cuddlesbeniwal@gmail.com>
2026-03-10 09:56:36 +0530

    UC2 2nd commit

--- src/DoubleRoom.java
+++
@@ -1,9 +0,0 @@
-class DoubleRoom extends Room {
-    public DoubleRoom() {
-        super(2, 400, 200);
-    }
-    @Override
-    public String getRoomType(){
-        return "Double room";
-    }
-}
--- src/UseCase2HotelBookingApp.java
+++ src/UseCase2HotelBookingApp.java
@@ -1,4 +1,58 @@
 public class UseCase2HotelBookingApp {
+    static abstract class Room {
+        private int beds;
+        private int size;
+        private double price;
+        public Room(int beds, int size, double price) {
+            this.beds = beds;
+            this.size = size;
+            this.price = price;
+        }
+        public int getBeds() {
+            return beds;
+        }
+        public int getSize() {
+            return size;
+        }
+        public double getPrice() {
+            return price;
+        }
+        public abstract String getRoomType();
+        public void displayRoomDetails() {
+            System.out.println("Room Type: " + getRoomType());
+            System.out.println("Beds: " + beds);
+            System.out.println("Size: " + size + " sq ft");
+            System.out.println("Price: $" + price);
+        }
+    }
+    static class SingleRoom extends Room {
+        public SingleRoom() {
+            super(1, 200, 100);
+        }
+        @Override
+        public String getRoomType() {
+            return "Single Room";
+        }
+    }
+    static class DoubleRoom extends Room {
+        public DoubleRoom() {
+            super(2, 400, 200);
+        }
+        @Override
+        public String getRoomType(){
+            return "Double room";
+        }
+    }
+    static class SuiteRoom extends Room {
+        public SuiteRoom(){
+            super(3, 750, 5000);
+        }
+        @Override
+        public String getRoomType(){
+            return "Suite room";
+        }
+    }
+
     public static void main(String[] args) {
         Room single = new SingleRoom();
         Room doubleRoom = new DoubleRoom();
