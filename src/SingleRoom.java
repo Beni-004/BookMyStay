017f29acb22b8417bb4050d147967a746799c48e
Aashmit Beniwal <cuddlesbeniwal@gmail.com>
2026-03-10 09:57:46 +0530

    Delete src directory

--- src/SingleRoom.java
+++
@@ -1,9 +0,0 @@
-class SingleRoom extends Room {
-    public SingleRoom() {
-        super(1, 200, 100);
-    }
-    @Override
-    public String getRoomType() {
-        return "Single Room";
-    }
-}
